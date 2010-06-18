grengojbo/opensips

commit b10edefde97d9c17e1f74a9c70f9c2f482220154

Fix chrash when received reply contains more streams than the INVITE.
git-svn-id: https://opensips.svn.sourceforge.net/svnroot/opensips/trunk@6812 689a6050-402a-0410-94f2-e92a70836424

modules/mediaproxy/mediaproxy.c
@@ -1578,7 +1578,7 @@ use_media_proxy(struct sip_msg *msg, char *dialog_id, ice_candidate_data *ice_da
         }
     }
 
-    for (i=0, j=1; i<session.stream_count; i++) {
+    for (i=0, j=1; i<session.stream_count && j<len; i++) {
         stream = session.streams[i];
         if (stream.transport != TSupported) {
             if (!stream.local_ip && removed_session_ip) {

commit 666fc6c3dfcd3b63e1bb6e639442bbf07ee4a3cb

Fix SDP mangling for unsupported streams. A bug was introduced in r6812 that prevented the original c line being added to the unsupported streams, this fixes it.
git-svn-id: https://opensips.svn.sourceforge.net/svnroot/opensips/trunk@6969 689a6050-402a-0410-94f2-e92a70836424

modules/mediaproxy/mediaproxy.c
@@ -1647,7 +1647,7 @@ use_media_proxy(struct sip_msg *msg, char *dialog_id, ice_candidate_data *ice_da
         }
     }
 
-    for (i=0, j=1; i<session.stream_count && j<len; i++) {
+    for (i=0, j=1; i<session.stream_count; i++) {
         stream = session.streams[i];
         if (stream.transport != TSupported) {
             if (!stream.local_ip && removed_session_ip) {
@@ -1662,6 +1662,10 @@ use_media_proxy(struct sip_msg *msg, char *dialog_id, ice_candidate_data *ice_da
             continue;
         }
 
+        if (j >= len) {
+            break;
+        }
+        
         if (!isnullport(stream.port)) {
             if (!replace_element(msg, &stream.port, &tokens[j])) {
                 LM_ERR("failed to replace port in media stream number %d\n", i+1);